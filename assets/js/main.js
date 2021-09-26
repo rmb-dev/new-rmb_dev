d4604bf928fae74ebaf25b464c92b220c16498d6
Roman <rombyr@outlook.com>
2021-09-25 23:44:24 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/* MENU SHOW Y HIDDEN */
 const   navMenu = document.getElementById('nav-menu'),
         navToggle = document.getElementById('nav-toggle'),
         navClose = document.getElementById('nav-close')
 
-/*===== MENU SHOW =====*/
+/* MENU SHOW */
 /* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
@@ -11,7 +11,7 @@ if(navToggle){
     })
 }
 
-/*===== MENU HIDDEN =====*/
+/* MENU HIDDEN */
 /* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', () =>{
@@ -29,7 +29,25 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/* ACCORDION SKILLS */
+const   skillsContent = document.getElementsByClassName('skills__content'),
+        skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
 
 
 /*==================== QUALIFICATION TABS ====================*/
